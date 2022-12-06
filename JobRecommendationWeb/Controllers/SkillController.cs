8f40a8b545396ec322312fdc2e83e0f7692665f9
Thai Duong <thaiduong7v@gmail.com>
2022-12-06 14:35:37 +0700

    tao dc modal

---
+++ JobRecommendationWeb/Controllers/SkillController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobRecommendationWeb.Controllers
+{
+    public class SkillController : Controller
+    {
+        private readonly JobRecommendationContext _context;
+
+        public SkillController(JobRecommendationContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Kinang> kinangs = _context.Kinangs.ToList();
+            return View(kinangs);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Kinang obj)
+        {
+            _context.Kinangs.Add(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Skill");
+        }
+
+        
+    }
+}
