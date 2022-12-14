ab3d5c59426775a279f6792736d377bb14f3b2b3
Thai Duong <thaiduong7v@gmail.com>
2022-12-14 23:24:07 +0700

    tao giao dien dang nhap, link duoc cac hyperlink

---
+++ JobRecommendationWeb/Controllers/PostController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobRecommendationWeb.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
