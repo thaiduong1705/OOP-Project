7357cec8082b7c18fe18a5301b363fc14be58735
thaiduong1705 <thaiduong7v@gmail.com>
2022-12-21 16:01:06 +0700

    them trang nhan vien, trang lich su lam viec, ho so cong ty, ho so ung vien

---
+++ JobRecommendationWeb/Migrations/20221221064037_AddDatabse.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobRecommendationWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDatabse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
